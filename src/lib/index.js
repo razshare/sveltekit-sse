5049902b8cc23faed6a453d49f7003b1095a88e7
Razvan <tangent.jotey@gmail.com>
2024-03-20 12:07:23 +0100

    chore(types): exporting types

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,4 +1,4 @@
 import { source } from './source.js'
 import { events } from './events.js'
-import * as types from './types.js'
-export { source, events, types }
+export * from './types.js'
+export { source, events }
