510661fee5d1ad600b2fa76ec5963c9ffe3f3ca2
Razvan <tangent.jotey@gmail.com>
2024-04-20 19:30:48 +0200

    chore(app): remove extra stuff from example

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -19,30 +19,8 @@ export function load({ url }) {
       )
       return previous
     })
-  /**
-   * @type {import('svelte/store').Readable<null|import('./events/+server.js').Quote>}
-   */
-  const quote2 = source(`/events?${searchParams}`, {
-    close({ connect }) {
-      console.log('reconnecting...')
-      connect()
-    },
-  })
-    .select('cat-quote')
-    .json(function or({ error, previous, raw }) {
-      console.log(
-        `Could not parse "${raw}" as json, reverting back to ${previous}. ${error}`,
-      )
-      return previous
-    })
-
-  const connection = source(`/events?${searchParams}`)
-  setTimeout(function after() {
-    connection.close()
-  }, 5000)
 
   return {
     quote,
-    quote2,
   }
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,6 @@
   import { enhance } from '$app/forms'
   export let data
   const quote = data?.quote
-  const quote2 = data?.quote2
 </script>
 
 <form use:enhance method="post">
@@ -12,4 +11,3 @@
 <h3>An International Cat Quote</h3>
 
 <span>{$quote?.value}</span><br />
-<span>{$quote2?.value}</span><br />
