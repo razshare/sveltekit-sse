16adbf47c2d55e628ec5360b40490ff426f1f8f9
Razvan <tangent.jotey@gmail.com>
2024-04-28 16:11:14 +0200

    chore(app): fixing exported names

--- src/lib/index.js
+++ src/lib/index.js
@@ -2,4 +2,4 @@ import { source } from './source.js'
 import { findBeacon } from './findBeacon.js'
 import { events, extend } from './events.js'
 export * from './types.js'
-export { source, events, extend, findBeacon as beacon }
+export { source, events, extend, findBeacon }
