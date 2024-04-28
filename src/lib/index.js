e64cc27fff0b38c9545f70a2a53433c0be629c72
Razvan <tangent.jotey@gmail.com>
2024-04-28 15:40:16 +0200

    feat(beacon): exposing beacon()

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,4 +1,5 @@
 import { source } from './source.js'
+import { beacon } from './beacon.js'
 import { events, extend } from './events.js'
 export * from './types.js'
-export { source, events, extend }
+export { source, events, extend, beacon }
