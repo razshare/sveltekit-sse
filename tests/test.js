8dcbfdf19878ba9b664c1a56481fc3e86454a507
Razvan Tanase <tangent.jotey@gmail.com>
2024-05-04 15:21:30 +0200

    chore(test): increasing delay a bit to ensure the environment doesn't close too soon

--- tests/test.js
+++ tests/test.js
@@ -6,7 +6,7 @@ test('reading at least 3 cat quotes from `/events` stream.', async function run(
   page,
 }) {
   await page.goto('/')
-  await delay(3000)
+  await delay(3500)
   const { counter } = await getPlaywrightState({ page })
   expect(counter).toBeGreaterThanOrEqual(3)
 })
