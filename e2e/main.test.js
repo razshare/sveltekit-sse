5ce0b03f533b97c67026c8becf8bf00a8e756bea
Razvan <razvan@razshare.dev>
2025-04-19 23:22:37 +0200

    chore(app): increasing leeway delay in main test for issue-43 and reverting path changes in jsconfig and vite config.

--- e2e/main.test.js
+++ e2e/main.test.js
@@ -6,13 +6,13 @@ test('Reading at least 3 cat quotes from `/events` stream.', async function run(
   page,
 }) {
   await page.goto('/')
-  await delay(3500)
+  await delay(5000)
   const { counter } = await getPlaywrightState({ page })
   expect(counter).toBeGreaterThanOrEqual(3)
 
   // Testing issue 43 https://github.com/razshare/sveltekit-sse/issues/43
   await page.goto('/issue-43')
-  await delay(4500)
+  await delay(5000)
   const { issue43 } = await getPlaywrightState({ page })
   expect(issue43.connections).toEqual(3)
   expect(issue43.disconnections).toEqual(3)
--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,5 @@
 import { sveltekit } from '@sveltejs/kit/vite'
 import { defineConfig } from 'vite'
-import path from 'path'
-import { fileURLToPath } from 'url'
-const file = fileURLToPath(import.meta.url)
-const dir = path.dirname(file).replace(/\\+/, '/')
-
 export default defineConfig({
   plugins: [sveltekit()],
-  resolve: {
-    alias: {
-      $lib: `${path.resolve(dir, './src')}`,
-    },
-  },
 })
