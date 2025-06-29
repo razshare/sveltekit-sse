e9638d6df2f2b8891dc633982307bc73c279a64c
Razvan <razvan@razshare.dev>
2025-06-29 19:39:09 +0200

    chore(app): exposing fetchEventSource.

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,4 +1,5 @@
 import { source } from './source.js'
 import { produce } from './produce.js'
+import { fetchEventSource } from './fetchEventSource.js'
 export * from './types.external.js'
-export { source, produce }
+export { source, produce, fetchEventSource }
